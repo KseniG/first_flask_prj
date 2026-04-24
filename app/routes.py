d9409ab176196d5c65be6e5ae38ee82256f5d9ea
Ksenia G <benvelopex612@gmail.com>
2026-04-24 11:37:21 +0300

    5 tasks

--- app/routes.py
+++ app/routes.py
@@ -49,3 +49,93 @@ def products():
                            user=user,
                            products=products,
                            price_threshold=price_threshold)
+
+# ========== ЗАДАНИЕ 3: НОВОСТНОЙ САЙТ ==========
+@app.route('/news')
+def news_home():
+    """Главная страница новостного раздела"""
+    user = {'username': 'Ksu'}
+    return render_template('news/index.html',
+                           title='Новости',
+                           site_name='Мой сайт',
+                           year=2026,
+                           user=user)
+
+@app.route('/news/list')
+def news_list():
+    """Список новостей"""
+    user = {'username': 'Ksu'}
+    # Данные: заголовок, просмотры
+    news_items = [
+        {'title': 'Запущен новый спутник', 'views': 1500},
+        {'title': 'Открытие кафе в центре', 'views': 870},
+        {'title': 'Выставка современного искусства', 'views': 2300},
+        {'title': 'Рекордный урожай яблок', 'views': 420},
+        {'title': 'Фестиваль уличной еды', 'views': 1200},
+    ]
+    return render_template('news/news.html',
+                           title='Список новостей',
+                           site_name='Мой сайт',
+                           year=2026,
+                           user=user,
+                           news=news_items)
+
+# ========== ЗАДАНИЕ 4: МИНИ-БЛОГ ==========
+# Данные статей (обычно хранятся в БД, но для примера – в коде)
+articles = {
+    1: {'title': 'Первый пост', 'content': 'Это текст первого поста. Привет, мир!'},
+    2: {'title': 'Flask – это просто', 'content': 'Flask позволяет быстро создавать веб-приложения на Python.'},
+    3: {'title': 'Шаблоны Jinja2', 'content': 'Jinja2 – мощный шаблонизатор с поддержкой условий и циклов.'},
+    4: {'title': 'Маршрутизация', 'content': 'С помощью декоратора @app.route можно связывать URL с функциями.'},
+}
+
+@app.route('/posts')
+def posts_list():
+    """Список всех статей"""
+    user = {'username': 'Ksu'}
+    # Преобразуем словарь в список для удобства в шаблоне
+    posts = [{'id': id, 'title': data['title']} for id, data in articles.items()]
+    return render_template('posts.html',
+                           title='Блог',
+                           site_name='Мой сайт',
+                           year=2026,
+                           user=user,
+                           posts=posts)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    """Страница отдельной статьи"""
+    user = {'username': 'Ksu'}
+    post = articles.get(id)
+    if not post:
+        return "Статья не найдена", 404
+    return render_template('post_detail.html',
+                           title=post['title'],
+                           site_name='Мой сайт',
+                           year=2026,
+                           user=user,
+                           post=post,
+                           post_id=id)
+
+# ========== ЗАДАНИЕ 5: СПИСОК РЕПОЗИТОРИЕВ ==========
+@app.route('/repos')
+def repos_list():
+    user = {'username': 'Ksu'}
+    repos = [
+        {'name': 'flask', 'language': 'Python', 'stars': 65000},
+        {'name': 'react', 'language': 'JavaScript', 'stars': 210000},
+        {'name': 'tensorflow', 'language': 'Python', 'stars': 180000},
+        {'name': 'bootstrap', 'language': 'CSS', 'stars': 164000},
+        {'name': 'my_small_lib', 'language': 'Python', 'stars': 45},
+        {'name': 'awesome_project', 'language': 'Go', 'stars': 320}
+    ]
+    stars_threshold = 100  # порог популярности
+    total_repos = len(repos)
+    return render_template('repos.html',
+                           title='Репозитории',
+                           site_name='Мой сайт',
+                           year=2026,
+                           user=user,
+                           repos=repos,
+                           stars_threshold=stars_threshold,
+                           total_repos=total_repos)
